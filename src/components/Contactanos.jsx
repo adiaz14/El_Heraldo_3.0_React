55095d80e99f6150868e4697deccb4821f77d9a2
adiaz14 <50351687+adiaz14@users.noreply.github.com>
2021-04-16 21:15:04 -0500

    Update Contactanos.jsx

--- src/components/Contactanos.jsx
+++ src/components/Contactanos.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 
-function Contactanos({contacto}) {
+function Contactanos() {
 
     return (
-        <div className="contacto">
-            {contacto}
+        <div class="container text-center">
+        <div class="container" style={{ height: '1px', backgroundColor: 'rgb(161, 16, 16)' }}></div>
+        <h1 class="text-center" style={{ fontFamily: 'Trebuchet MS', color: 'rgb(161, 16, 16)' }}>
+          <b>El Heraldo</b>
+        </h1>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 text-center">
+              <div class="row">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4"><br />
+                  <p style={{ fontSize: '9px' }} class="text-secundary">EL HERALDO S.A. Prohibida la reproducción y utilización,
+                  total o parcial, de
+                  los contenidos en cualquier forma o modalidad, sin previa, expresa y escrita autorización,
+                  incluyendo su mera reproducción y/o puesta a disposición con fines comerciales, directa o
+            indirectamente lucrativos. 2000 - 2021 ©</p>
+                </div>
+                <div class="col-sm-4"><b>Siganos en nuestras redes sociales:</b><br />
+                  <img width="80%" height="80%"
+                    src="https://isanidad.com/wp-content/uploads/2019/01/Siguenos-en-las-redes-sociales.jpg" />
+                </div>
+              </div>
+              <p><strong>PBX:</strong> (57) (5) 3715000 <strong>Línea de atención:</strong> (57) (5) 313-3400 /
+        300-910-9968 Calle 53B N° 46 - 25, Barranquilla - Atlántico Oficina
+        Bogotá: Dirección Calle 88 No. 13 A 07 <strong>Teléfono:</strong> (1) 218 5733</p><br /><br />
+            </div>
+          </div>
         </div>
+
+
+
+
+      </div>
     );
 };
 export default Contactanos;
