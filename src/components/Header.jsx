ef541ef37fe90030026338b4f2f3fab9bb86a370
adiaz14 <50351687+adiaz14@users.noreply.github.com>
2021-04-16 21:15:30 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
-function Header({ header }) {
-
+function Header() {
+    const estiloAnuncios = { lineHeight: '300%', fontSize: '10px' };
+    const estiloImagenAnuncios = { width: '10%', marginLeft: '20px' };
+    const estiloTituloPrincipal = { color: 'rgb(161, 16, 16)' };
 
     return (
 
-        <div className="Header">
-            {header}
+        <div>
+
+            <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#anuncios">Mostrar/quitar
+  anuncios</button>
+            <div id="anuncios" style={estiloAnuncios} class="container-fluid text-left bg-danger collapse">
+                <b>Anuncios</b>
+                <a href="#">
+                    <img style={estiloImagenAnuncios}
+                        src="https://tienda.elheraldo.co/1107-home_default/suscripcion-18-meses-el-heraldo.jpg" />
+                </a>
+            </div>
+            <div class="container text-center">
+                <p>Miércoles, 24 de Febrero, 2021</p>
+                <h1 style={estiloTituloPrincipal}>EL HERALDO</h1>
+            </div>
+
         </div>
-       
+
     );
 }
 
