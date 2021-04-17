e4425ee695eda4828e12f78002b5a5e2665d9eb2
adiaz14 <50351687+adiaz14@users.noreply.github.com>
2021-04-16 21:14:43 -0500

    Update Comentarios.jsx

--- src/components/Comentarios.jsx
+++ src/components/Comentarios.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
-function Comentarios({ formulario }) {
+function Comentarios() {
 
 
     return (
 
-        <div className="formulario">
-            {formulario}
+     
+        <div style={{ marginBottom: '50px', textAlign: 'center' }} class="container text-center">
+          <form>
+            <div class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+              <input id="email" type="text" class="form-control" name="email" placeholder="Usuario/correo" />
+            </div>
+            <div class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+              <input id="password" type="password" class="form-control" name="password" placeholder="Contraseña" />
+            </div>
+            <br />
+            <div class="input-group">
+              <span class="input-group-addon">Escribanos!</span>
+              <input id="msg" type="text" class="form-control" name="msg" placeholder="Dejenos su comentario!" />
+            </div>
+          </form>
+          <button style={{ marginTop: '15px' }} type="button" class="btn btn-danger">Enviar</button>
         </div>
+
     );
 }
 
