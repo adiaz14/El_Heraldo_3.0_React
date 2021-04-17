004d0443315cc75e9843dcd918fc531ae79b05ec
adiaz14 <50351687+adiaz14@users.noreply.github.com>
2021-04-16 21:16:32 -0500

    Update MenuSuperior.jsx

--- src/components/MenuSuperior.jsx
+++ src/components/MenuSuperior.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 
-function MenuSuperior({ menuSuperior }) {
+function MenuSuperior() {
 
 
     return (
 
         <div className="MenuSuperior">
-            {menuSuperior}
+                <div class="container-fluid">
+          <div class="navbar-header">
+            <a class="navbar-brand text-danger" href="#">El Heraldo</a>
+          </div>
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Inicio</a></li>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Servicios<span
+              class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Entregas a domicilio</a></li>
+                <li><a href="#">Solicite subscripcion por correo</a></li>
+                <li><a href="#">Como obtener nuestra app</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Quienes somos</a></li>
+            <li><a href="#">Contactenos</a></li>
+          </ul>
+        </div>
         </div>
        
     );
