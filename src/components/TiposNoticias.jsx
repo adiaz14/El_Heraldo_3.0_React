1aad24c7a897e2211e5dfa82ddaf94c486045b0c
adiaz14 <50351687+adiaz14@users.noreply.github.com>
2021-04-16 21:17:33 -0500

    Update TiposNoticias.jsx

--- src/components/TiposNoticias.jsx
+++ src/components/TiposNoticias.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 
-function TiposNoticias({ tipo }) {
+function TiposNoticias() {
 
 
     return (
 
-        <div className="tipo">
-            {tipo}
+        <div class="container text-center">
+        <div class="row">
+          <div class="col-sm-2"></div>
+          <div class="col-sm-8">
+            <ul class="nav nav-pills">
+              <li class="active"><a href="#">LOCALES</a></li>
+              <li><a href="#">NOTICIAS</a></li>
+              <li><a href="#">JUDICIALES</a></li>
+              <li><a href="#">DEPORTES</a></li>
+              <li><a href="#">CLASIFICADOS</a></li>
+              <li><a href="#">POLITICA</a></li>
+            </ul>
+          </div>
+          <div class="col-sm-2"></div>
         </div>
+      </div>
+
        
     );
 }
